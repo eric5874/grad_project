d6db7d1dbcfb17e5cc02d932c99a98e83fe6e9e0
hibana2077 <hibana2077@gmail.com>
2024-05-26 14:57:43 +0800

    chore: Update Home page with resource links and descriptions

--- src/web/Home.py
+++ src/web/Home.py
@@ -33,7 +33,14 @@ else:
         st.write('### 歡迎來到資工所資源網站')
         st.write('這是一個為資工所學生設計的資源網站')
         st.write('你可以在這裡找到許多資源，例如：')
-        st.write('- 課程筆記')
-        st.write('- 考古題')
-        st.write('- 程式範例')
-        st.write('- 等等')
+        data_structure = st.expander('資料結構')
+        algorithm = st.expander('演算法')
+        linear_algebra = st.expander('線性代數')
+        discrete_math = st.expander('離散數學')
+        operating_system = st.expander('作業系統')
+        computer_architecture = st.expander('計算機結構')
+        
+        data_structure.write("""
+        1. [資料結構可視化](https://visualgo.net/en)
+        2. [Fundamentals-of-Data-Structures-in-C](https://caucse.club/wp-content/uploads/2022/05/Fundamentals-of-Data-Structures-in-C-Ellis-Horowitz-Sartaj-Sahni-etc.-.pdf)
+        3. [交大彭文志教授ocw](https://ocw.nycu.edu.tw/?course_page=all-course%2Fcollege-of-computer-science%2F%E8%B3%87%E6%96%99%E7%B5%90%E6%A7%8B-data-structure-101%E5%AD%B8%E5%B9%B4%E5%BA%A6-%E8%B3%87%E8%A8%8A%E5%B7%A5%E7%A8%8B%E5%AD%B8%E7%B3%BB-%E5%BD%AD%E6%96%87%E5%BF%97%E8%80%81%E5%B8%AB)""")
