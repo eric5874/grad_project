e67516f6a3c0b7110e1fcf92bdca92346ff6a8be
hibana2077 <hibana2077@gmail.com>
2024-05-26 14:31:29 +0800

    chore: Update Home page with resource links and descriptions

--- src/web/Home.py
+++ src/web/Home.py
@@ -20,5 +20,20 @@ if not st.session_state['logged_in']:
             st.error('Invalid username or password')
 
 else:
-    st.title('Home')
-    st.write('Welcome to the home page!')
+    st.title('資工所資源網站')
+    col1, col2 = st.columns([1, 4])
+
+    with col1:
+        st.write('### 目錄')
+        st.write('1. [首頁](/)')
+        st.write('2. [資源](/resources)')
+        st.write('3. [關於](/about)')
+
+    with col2:
+        st.write('### 歡迎來到資工所資源網站')
+        st.write('這是一個為資工所學生設計的資源網站')
+        st.write('你可以在這裡找到許多資源，例如：')
+        st.write('- 課程筆記')
+        st.write('- 考古題')
+        st.write('- 程式範例')
+        st.write('- 等等')
