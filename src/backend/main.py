611e188dfb4a7ae426c8625980006674e8cd6cd6
hibana2077 <hibana2077@gmaill.com>
2024-06-02 11:54:07 +0800

    chore: Update main.py to use environment variable for host

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,8 +1,17 @@
+'''
+Author: hibana2077 hibana2077@gmaill.com
+Date: 2024-05-24 02:35:11
+LastEditors: hibana2077 hibana2077@gmaill.com
+LastEditTime: 2024-06-02 11:53:33
+FilePath: /grad_project/src/backend/main.py
+Description: This is main FastAPI backend file
+'''
 from fastapi import FastAPI
 import pymongo
 import uvicorn
 import os
 
+HOST = os.getenv("HOST", "127.0.0.1")
 MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
 
 client = pymongo.MongoClient(MONGO_URL)
@@ -33,4 +42,4 @@ def read_login(username: str, password: str):
     return {"message": "Invalid username or password"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host=HOST, port=8000)
