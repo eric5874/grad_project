20464a3bcac64ee34c9a5ef7bc2153c6f9dc1e85
potter04771 <bao148815@gmail.com>
2024-11-14 15:18:10 +0800

    newsncku

--- src/web/pages/news.py
+++ src/web/pages/news.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from news_utl.ntu_csie import fetch_ntu_csie_news
 from news_utl.nycu_csie import fetch_nycu_admissions_info
+from news_utl.nckunews import fetch_announcements
 
 def display_news():
     st.write('### 最新消息')
@@ -8,6 +9,7 @@ def display_news():
     # Create Markdown table header with date column for NTU
     md_template_ntu = """| NTU 標題 | 日期 | 連結 |\n| ---- | ---- | ---- |\n"""
     md_template_nycu = """| NYCU 標題 | 連結 |\n| ---- | ---- |\n"""
+    md_template_ncku = """| NCKU 標題 | 日期 | 連結 |\n| ---- | ---- | ---- |\n"""
     
     # Fetch NTU news and include date
     ntu_news_df = fetch_ntu_csie_news()
@@ -20,10 +22,15 @@ def display_news():
     for i, row in nycu_news_df.iterrows():
         full_link = f"{row['link']}"
         md_template_nycu += f"| {row['title']} | [Link]({full_link}) |\n"
+
+    ncku_news_df = fetch_announcements()
+    for i, row in ncku_news_df.iterrows():
+        full_link = f"{row['link']}"
+        md_template_ncku += f"| {row['title']} | {row['date']} | [Link]({full_link}) |\n"
     
     # Tabs
 
-    ntu_tab, nycu_tab = st.tabs(['NTU', 'NYCU'])
+    ntu_tab, nycu_tab, ncku_tab= st.tabs(['NTU', 'NYCU','nCku'])
 
     # Display both NTU and NYCU news tables
     with ntu_tab:
@@ -33,5 +40,8 @@ def display_news():
     with nycu_tab:
         st.markdown("### NYCU 最新消息")
         st.markdown(md_template_nycu)
+    with ncku_tab:
+        st.markdown("### 成功大學 最新消息")
+        st.markdown(md_template_ncku)
 
 display_news()
