796d86ec09bba84c4353c6d3f3e7976801e96ede
potter04771 <bao148815@gmail.com>
2024-11-18 16:18:35 +0800

    ncubugfix

--- src/web/pages/news.py
+++ src/web/pages/news.py
@@ -2,7 +2,7 @@ import streamlit as st
 from news_utl.ntu_csie import fetch_ntu_csie_news
 from news_utl.nycu_csie import fetch_nycu_admissions_info
 from news_utl.nckunews import fetch_announcements
-from news_utl.nckunews import fetch_ncu_news
+from news_utl.ncu_csie import fetch_ncu_news
 
 def display_news():
     st.write('### 最新消息')
