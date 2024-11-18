98eb9b80f44230ca5f232a9c8b77a569fb32a6b3
potter04771 <bao148815@gmail.com>
2024-11-18 16:14:01 +0800

    news_update1118

--- src/web/news_utl/ntu_csie.py
+++ src/web/news_utl/ntu_csie.py
@@ -11,14 +11,14 @@ def fetch_ntu_csie_news():
     news = soup.find_all('div', class_='w-annc')[0]
 
     news_df = pd.DataFrame(columns=['title', 'date', 'link'])
-
+    #full_link = f"https://www.csie.ncku.edu.tw{link}"
     dates = news.find_all('span', class_='w-annc__postdate')
     news_items = news.find_all('a')
     for n, d in zip(news_items, dates):
         title = n.text
         date = d.text
         link = n['href']
-        news_df = news_df._append({'title': title, 'date': date, 'link': link}, ignore_index=True)
+        news_df = news_df._append({'title': title, 'date': date, 'link': f"https://www.csie.ntu.edu.tw{link}"}, ignore_index=True)
 
     return news_df
 
