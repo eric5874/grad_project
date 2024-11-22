72d1e031c26c97d2cfb6f33fcc62daa3ce821790
hibana2077 <hibana2077@gmaill.com>
2024-11-22 14:44:32 +0800

    Update chat model version in Chat.py and refine prompt template

--- src/web/pages/Chat.py
+++ src/web/pages/Chat.py
@@ -14,13 +14,12 @@ OLLAMA_SERVER = os.getenv("OLLAMA_SERVER", "http://localhost:11434")
 BACKEND_SERVER = os.getenv("BACKEND_SERVER", "http://localhost:8081")
 OPEN_API_KEY = os.getenv("OPENAI_API_KEY", "sk_test_1234567890")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY", "gsk_test_1234567890")
-TRANSLATOR_PROVIDER = os.getenv("TRANSLATOR_PROVIDER", "google")
 EXTRACT_PROMPT = ChatPromptTemplate.from_template(
     "You are an Top algorithm, you need to according to user input extract information from the content. user input: {user_input}, content: {content}"
 )
 
 if 'chat_model' not in st.session_state:
-    st.session_state['chat_model'] = "llama3.1:70b"
+    st.session_state['chat_model'] = "llama-3.2-90b-text-preview-groq"
 
 def get_all_embeddings() -> list:
     response = requests.get(f"{BACKEND_SERVER}/embedding_count")
